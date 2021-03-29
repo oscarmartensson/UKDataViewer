7aab6e1618b3d916ee2df5ffccd2044cad7c3e31
oscarmartensson <oscar.mattias.martensson@hotmail.com>
2021-03-29 21:57:12 +0200

    Add a Postcode client that interacts with the Postcode.io REST API. Currently the client only bulk fetches longitude and latitude for post codes that will be used for computing people geographically close to each other. Also no error handling is present, but will be added later.

---
+++ UKDataViewer/PostcodeResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UKDataViewer
+{
+    [Serializable]
+    public class BulkQueryResult<TQuery, TResult> where TResult : class
+    {
+        public TQuery Query { get; set; }
+        public TResult Result { get; set; }
+    }
+
+    [Serializable]
+    public class PostcodeIOResponse<T>
+    {
+        public int Status { get; set; }
+        public T Result { get; set; }
+    }
+
+    [Serializable]
+    public class LongLat
+    {
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
---
+++ UKDataViewer/PostcodesClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using RestSharp;
+
+
+namespace UKDataViewer
+{
+    class PostcodesClient
+    {
+        private string endpoint = "https://api.postcodes.io";
+
+        private RestClient client;
+
+        public PostcodesClient()
+        {
+            client = new RestClient(endpoint);
+        }
+
+        public List<BulkQueryResult<string, LongLat>> BulkPostcodeLookup(List<string> postcodes)
+        {
+            var request = new RestRequest("postcodes?filter=longitude,latitude", Method.POST)
+            {
+                RequestFormat = DataFormat.Json
+            };
+            request.AddJsonBody(new { postcodes });
+
+            var response = client.Execute<PostcodeIOResponse<List<BulkQueryResult<string, LongLat>>>>(request);
+
+            if (response.ErrorException != null)
+            {
+                 // Error handling
+            }
+            if (response.Data != null && response.Data.Status != 200)
+            {
+                // Some error due to server not responding.
+            }
+            return response.Data.Result;
+        }
+    }
+}
